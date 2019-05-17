5de8e4b25d1fd4d1a0291f7b6ea02bbc7bac1ff2
Administrator <499974664@qq.com>
2019-05-17 09:34:48 +0800

    init

---
+++ src/test/java/com/hust/nb/NbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hust.nb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
